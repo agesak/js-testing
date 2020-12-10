9fd82b9953d7fdeae2756c31bb435f9bbb6ccb65
Kareha Agesa <kmaagesa@gmail.com>
2020-12-09 22:22:44 -0800

    pangram tests passing

--- src/is_pangram.js
+++ src/is_pangram.js
@@ -1,4 +1,46 @@
 const isPangram = function(text) {
+  text = text.toLowerCase()
+  const alphabet = {
+    "a": 0,
+    "b": 0,
+    "c": 0,
+    "d": 0,
+    "e": 0,
+    "f": 0,
+    "g": 0,
+    "h": 0,
+    "i": 0,
+    "j": 0,
+    "k": 0,
+    "l": 0,
+    "m": 0,
+    "n": 0,
+    "o": 0,
+    "p": 0,
+    "q": 0,
+    "r": 0,
+    "s": 0,
+    "t": 0,
+    "u": 0,
+    "v": 0,
+    "w": 0,
+    "x": 0,
+    "y": 0,
+    "z":0
+    };
+  for (const char of text ){
+    if (Number.isInteger(alphabet[char])){
+      alphabet[char] += 1;
+    }
+  }
+
+  const nonOccurersArray = Object.values(alphabet).filter(function(x) {return x == 0});
+
+  if (nonOccurersArray.length == 0){
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
--- test/is_pangram.test.js
+++ test/is_pangram.test.js
@@ -6,58 +6,37 @@ describe('isPangram()', () => {
   });
 
   test('works with a pangram with only lower case', () => {
-    // Arrange
     const text = 'the quick brown fox jumps over the lazy dog';
-
-    // Act
-
-    // Assert
+    expect(isPangram(text)).toBe(true);
   });
 
   test('works with "abcdefghijklmnopqrstuvwxyz"', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'abcdefghijklmnopqrstuvwxyz';
+    expect(isPangram(text)).toBe(true);
   });
 
   test("missing character 'x'", () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'abcdefghijklmnopqrstuvwyz';
+    expect(isPangram(text)).toBe(false);
   });
 
   test('empty sentence', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = ''
+    expect(isPangram(text)).toBe(false);
   });
 
   test('pangram with underscores instead of spaces works', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'the_quick_brown_fox_jumps_over_the_lazy_dog';
+    expect(isPangram(text)).toBe(true);
   });
 
   test('pangram with numbers', () => {
-    // Arrange
-
-    // Act
-
-    // Assert
-
+    const text = 'the1quick2brown3fox4jumps5over6the7lazy8dog';
+    expect(isPangram(text)).toBe(true);
   });
-
-  // Write your own test case
+  
+  test('pangram with upcase', () => {
+    const text = 'THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG';
+    expect(isPangram(text)).toBe(true);
+  });
 });
